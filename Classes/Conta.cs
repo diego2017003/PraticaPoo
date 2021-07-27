86dc2b4b89796396ef9f2645541b69828c13c5fd
diego2017003 <30417399+diego2017003@users.noreply.github.com>
2021-07-27 16:31:44 -0300

    Add files via upload

---
+++ Classes/Conta.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeConta.Classes
+{
+    class Conta
+    {
+    private TipoConta tipoconta{get; set;}
+    private double saldo {get; set;}
+    private double credito{get; set;}
+    private string nome {get; set;}
+    public Conta(TipoConta tipoconta, string nome, double saldo, double credito) {
+        this.credito = credito;
+        this.nome = nome;
+        this.saldo = saldo;
+        this.tipoconta = tipoconta;
+    }
+        public bool Sacar(double valorSaque)
+        {
+            // Validação de saldo suficiente
+            if (this.saldo - valorSaque < (this.credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            this.saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.nome, this.saldo);
+          
+            return true;
+        }
+        public void VisualizarSaldo()
+        {
+            Console.WriteLine("Seu saldo é {0}", saldo);
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.nome, this.saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.tipoconta + " | ";
+            retorno += "Nome " + this.nome + " | ";
+            retorno += "Saldo " + this.saldo + " | ";
+            retorno += "Crédito " + this.credito;
+            return retorno;
+        }
+    }
+
+
+}
+
